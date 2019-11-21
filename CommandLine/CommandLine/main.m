f279052867d1ae3c2fdea800fa78872738052b25
Knar <knarxr@protonmail.com>
2019-11-21 15:59:56 -0600

    enums and switches

--- CommandLine/CommandLine/main.m
+++ CommandLine/CommandLine/main.m
@@ -1,14 +1,24 @@
 #import <Foundation/Foundation.h>
 
+enum popcornSizes {
+    smallPopcorn = 1,
+    mediumPopcorn = 2,
+    largePopcorn = 3
+};
+
 int main(int argc, const char * argv[]) {
-    bool testBool = true;
-    BOOL otherBool = YES;
-    
-    if (testBool == otherBool) {
-        NSLog(@"yes");
-    }
-    else {
-        NSLog(@"no");
+    int popcornSize = smallPopcorn;
+    float price;
+    switch(popcornSize) {
+        case smallPopcorn:
+            price = 1.5;
+            break;
+        case mediumPopcorn:
+            price = 3;
+            break;
+        case largePopcorn:
+            price = 4.5;
+            break;
     }
     return 0;
 }
