07473cb636fe3f23e91ddaf9814d7cc20deb79f0
Knar <knarxr@protonmail.com>
2019-12-04 10:03:52 -0600

    Trying to understand why my floats are turned into shorts

--- CommandLine/CommandLine/main.m
+++ CommandLine/CommandLine/main.m
@@ -2,18 +2,24 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSNumber *myNumber = @0.0F;
         NSDictionary *orderDict = @{
             @"burgers": @5,
             @"shakes": @3,
             @"customers": @4,
             @"isTakeout": @NO,
-            @"subtotal": @0.0,
+            @"subtotal": @0.0F,
         };
         float burgerPrice = 4;
         float shakePrice = 3;
         float subtotal;
         
         subtotal = (burgerPrice * [[orderDict valueForKey:@"burgers"]intValue]) + (shakePrice * [[orderDict valueForKey:@"shakes"]intValue]);
+        
+        NSMutableDictionary *outputDict = [NSMutableDictionary dictionaryWithDictionary:orderDict];
+        
+        [outputDict setValue:@(subtotal) forKey:@"subtotal"];
     }
+    
     return 0;
 }
