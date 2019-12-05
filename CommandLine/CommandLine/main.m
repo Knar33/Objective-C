64e3d19153495526a263f6978529d48cce147186
Knar <knarxr@protonmail.com>
2019-12-05 09:56:30 -0600

    Pointers

--- CommandLine/CommandLine/main.m
+++ CommandLine/CommandLine/main.m
@@ -2,11 +2,27 @@
 #import "MyFirstClass.h"
 #import "NSArray+Deduper.h"
 
+void increment(int i);
+void incrementPointer(int *i);
+
 int main(int argc, const char * argv[]) {
-    
-    NSArray *rawArray = @[@"val1", @"val2", @"val1", @"val3"];
-    
-    rawArray = [rawArray Dedupe];
-    
-    return 0;
+    int i = 10;
+    NSLog(@"memory address for i: %p", &i);
+    NSLog(@"value for  i: %i", i);
+    increment(i);
+    NSLog(@"memory address for i: %p", &i);
+    NSLog(@"value for i: %i", i);
+    incrementPointer(&i);
+    NSLog(@"memory address for i: %p", &i);
+    NSLog(@"value for i: %i", i);
+}
+
+void increment(int i) {
+    NSLog(@"memory address for i: %p", &i);
+    i++;
+}
+
+void incrementPointer(int *i) {
+    NSLog(@"memory address for i: %p", &i);
+    *i = *i+1;
 }
