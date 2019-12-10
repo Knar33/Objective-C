c47acfe00b80459b2cd64a072ec8ff7fb5b88ad8
Knar <knarxr@protonmail.com>
2019-12-10 13:44:52 -0600

    Added a page with a button

--- Algorythm/Algorythm/ViewController.m
+++ Algorythm/Algorythm/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.aButton setTitle:@"Press me!" forState:UIControlStateNormal];
 }
 
 
